2fe7e3f4d22136fe87aaaacae6b407c0dbdc6ee1
Raktimmaity <raktimmaity2003@gmail.com>
2023-12-09 20:45:13 +0530

    .

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ contact.addEventListener('submit', function (e) {
     alert(`Sorry Request cannot be accepted due to some technical errors`);
 })
 
-let loader = document.getElementById("loader");
+let loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
     loader.style.display = "none";
 })
